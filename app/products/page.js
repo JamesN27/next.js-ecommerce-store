1ddd83132d5afd352be67079f6cb4f8a727f5415
James Henning <jamesniklas@hotmail.de>
2023-05-25 01:39:28 +0200

    Delete worn jersey pictures from products page

--- app/products/page.js
+++ app/products/page.js
@@ -11,51 +11,6 @@ const jerseys = [
   { id: 7, name: 'Sacramento Kings' },
 ];
 
-const jerseysPerson = [
-  {
-    id: 1,
-    name: 'denverperson',
-    originalName: 'Denver Nuggets',
-    personName: 'Denver Person',
-  },
-  {
-    id: 2,
-    name: 'lakersperson',
-    originalName: 'Los Angeles Lakers',
-    personName: 'Lakers Person',
-  },
-  {
-    id: 3,
-    name: 'sanantonioperson',
-    originalName: 'San Antonio Spurs',
-    personName: 'San Antonio Person',
-  },
-  {
-    id: 4,
-    name: 'milwaukeeperson',
-    originalName: 'Milwaukee Bucks',
-    personName: 'Milwaukee Person',
-  },
-  {
-    id: 5,
-    name: 'atlantaperson',
-    originalName: 'Atlanta Hawks',
-    personName: 'Atlanta Person',
-  },
-  {
-    id: 6,
-    name: 'detroitperson',
-    originalName: 'Detroit Pistons',
-    personName: 'Detroit Person',
-  },
-  {
-    id: 7,
-    name: 'sacramentoperson',
-    originalName: 'Sacramento Kings',
-    personName: 'Sacramento Person',
-  },
-];
-
 export default function ProductsPage() {
   return (
     <main>
@@ -74,25 +29,6 @@ export default function ProductsPage() {
           </Link>
         </div>
       ))}
-      <div>
-        {jerseysPerson.map((jersey) => (
-          <div key={`jersey-${jersey.name}`}>
-            <br />
-            <p>{jersey.originalName}</p>
-            <Link
-              href={`/products/${encodeURIComponent(jersey.originalName)}`}
-              as={`/products/${encodeURIComponent(jersey.name)}`}
-            >
-              <Image
-                src={`/images/${encodeURIComponent(jersey.name)}.png`}
-                alt={jersey.originalName}
-                width={300}
-                height={300}
-              />
-            </Link>
-          </div>
-        ))}
-      </div>
     </main>
   );
 }
