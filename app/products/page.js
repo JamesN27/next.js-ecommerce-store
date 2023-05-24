5364eb8c228d79171c7476ad17bbce7eda12aa45
James Henning <jamesniklas@hotmail.de>
2023-05-24 18:18:01 +0200

    Insert worn jersey pictures

--- app/products/page.js
+++ app/products/page.js
@@ -2,12 +2,22 @@ import Image from 'next/image';
 
 const jerseys = [
   { id: 1, name: 'denver' },
-  { id: 2, name: 'milwaukee' },
   { id: 3, name: 'lakers' },
+  { id: 6, name: 'sanantonio' },
+  { id: 2, name: 'milwaukee' },
   { id: 4, name: 'atlanta' },
   { id: 5, name: 'detroit' },
-  { id: 6, name: 'sanantonio' },
   { id: 7, name: 'sacramento' },
+];
+
+const jerseysPerson = [
+  { id: 1, name: 'denverperson' },
+  { id: 3, name: 'lakersperson' },
+  { id: 6, name: 'sanantonioperson' },
+  { id: 2, name: 'milwaukeeperson' },
+  { id: 4, name: 'atlantaperson' },
+  { id: 5, name: 'detroitperson' },
+  { id: 7, name: 'sacramentoperson' },
 ];
 
 export default function ProductsPage() {
@@ -25,6 +35,19 @@ export default function ProductsPage() {
           />
         </div>
       ))}
+      <div>
+        {jerseysPerson.map((jersey) => (
+          <div key={`jersey-${jersey.id}`}>
+            <br />
+            <Image
+              src={`/images/${jersey.name}.png`}
+              alt={jersey.name}
+              width={300}
+              height={300}
+            />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
