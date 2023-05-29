ca4d45180f4e7b6e03c7c0b7b5f275175cb0143c
James Henning <jamesniklas@hotmail.de>
2023-05-30 01:57:11 +0200

    Add heroimage to products page

--- app/products/page.js
+++ app/products/page.js
@@ -1,17 +1,27 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { getJerseys } from '../database/jerseys';
+import style from './productlayout.module.scss';
 
 const jerseys = getJerseys();
 
 export default function ProductsPage() {
   return (
     <main>
+      <div>
+        <Image
+          className={style.productsImage}
+          src="/images/productsimage.png"
+          alt="Hero Image"
+          layout="fill"
+          priority={true}
+        />
+      </div>
       {jerseys.map((jersey) => (
         <div key={`jersey-${jersey.id}`}>
           <br />
           <p>{jersey.name}</p>
-          <div> {jersey.price}</div>
+          <div>{jersey.price}</div>
           <Link href={`/products/${encodeURIComponent(jersey.name)}`}>
             <Image
               src={`/images/${encodeURIComponent(jersey.name)}.png`}
