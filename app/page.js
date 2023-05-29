82a113c968e5d07be40ac7929086751dd958fce4
James Henning <jamesniklas@hotmail.de>
2023-05-30 00:55:30 +0200

    Fix hydration issue

--- app/page.js
+++ app/page.js
@@ -14,6 +14,18 @@ export default function HomePage() {
           priority={true}
         />
       </div>
+      <div className={style.companyBox}>
+        <h2>Welcome to Pro Hoops Apparel!</h2>
+        <p>
+          Your destination for NBA Jerseys! Step into the world of basketball
+          fashion and embrace the spirit of the game. <br /> Discover the latest
+          jerseys worn by your favorite NBA players, from the electrifying dunks
+          of LeBron James to the mesmerizing handles of Stephen Curry. <br />{' '}
+          Join the passionate NBA community, elevate your style, and be a part
+          of basketball history. Shop now at Pro Hoops Apparel and score your
+          own piece of NBA greatness!
+        </p>
+      </div>
     </main>
   );
 }
