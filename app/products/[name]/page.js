3725d856160be5040274cdf5abd0418177f26a6c
James Henning <jamesniklas@hotmail.de>
2023-06-07 12:59:03 +0200

    Add single products background picture and styling

--- app/products/[name]/page.js
+++ app/products/[name]/page.js
@@ -2,9 +2,6 @@ import Image from 'next/image';
 import { getJerseys, getJerseysPerson } from '../../database/jerseys.ts';
 import AddToCart from './AddToCart';
 
-// const jerseys = await getJerseys();
-// const jerseysPerson = getJerseysPerson();
-
 export const dynamic = 'force-dynamic';
 
 export default async function SingleProductsPage({ params }) {
@@ -28,32 +25,54 @@ export default async function SingleProductsPage({ params }) {
     );
   }
 
+  // Forgive me but I had to use CSS inline styling here, do not ask me why, there was just no other way to get this god damned background picture to work
+
+  const heroImageStyle = {
+    height: 400,
+  };
+
+  const backgroundImageStyle = {
+    content: '',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundImage: 'url("/images/test.png")',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    zIndex: -1,
+  };
+
   return (
     <main>
-      <h1>{decodedName}</h1>
-      <h2>Ball up with this {jersey.name} and enjoy the great game</h2>
-      {jersey && (
-        <div>
-          <div> {jersey.price} </div>
-          <Image
-            src={`/images/${encodeURIComponent(jersey.name)}.png`}
-            alt={jersey.name}
-            width={300}
-            height={300}
-          />
-          <AddToCart jersey={jersey} />
-        </div>
-      )}
-      {jerseyPerson && (
-        <div>
-          <Image
-            src={`/images/${encodeURIComponent(jerseyPerson.name)}.png`}
-            alt={jerseyPerson.originalName}
-            width={300}
-            height={300}
-          />
-        </div>
-      )}
+      <div style={heroImageStyle}>
+        <div style={backgroundImageStyle} />
+        <h1>{decodedName}</h1>
+        <h2>Ball up with this {jersey?.name} and enjoy the great game</h2>
+        {jersey && (
+          <div>
+            <div>{jersey.price}</div>
+            <Image
+              src={`/images/${encodeURIComponent(jersey.name)}.png`}
+              alt={jersey.name}
+              width={300}
+              height={300}
+            />
+            <AddToCart jersey={jersey} />
+          </div>
+        )}
+        {jerseyPerson && (
+          <div>
+            <Image
+              src={`/images/${encodeURIComponent(jerseyPerson.name)}.png`}
+              alt={jerseyPerson.originalName}
+              width={300}
+              height={300}
+            />
+          </div>
+        )}
+      </div>
     </main>
   );
 }
