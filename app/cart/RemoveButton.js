440da435e25f70e98e130154dc58b1a2b677e2c6
James Henning <jamesniklas@hotmail.de>
2023-06-09 10:48:00 +0200

    Add possibility to remove certain amounts of products from cart

--- app/cart/RemoveButton.js
+++ app/cart/RemoveButton.js
@@ -1,19 +1,35 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { removeJerseyFromCart } from './actions';
 
 export default function RemoveFromCart(props) {
+  const [quantityToRemove, setQuantityToRemove] = useState(1); // Initial quantity to remove is set to 1
   const router = useRouter();
 
+  const handleRemove = async (event) => {
+    event.preventDefault();
+    router.refresh();
+    await removeJerseyFromCart(props.jerseyId, quantityToRemove);
+  };
+
+  const handleQuantityChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    if (Number.isInteger(value) && value > 0) {
+      setQuantityToRemove(value);
+    }
+  };
+
   return (
-    <form
-      action={async () => {
-        router.refresh();
-        await removeJerseyFromCart(props.jerseyId);
-      }}
-    >
-      <button>Remove</button>
+    <form onSubmit={handleRemove}>
+      <input
+        type="number"
+        min="1"
+        value={quantityToRemove}
+        onChange={handleQuantityChange}
+      />
+      <button type="submit">Remove</button>
     </form>
   );
 }
--- app/cart/actions.js
+++ app/cart/actions.js
@@ -4,18 +4,25 @@ import { cookies } from 'next/headers';
 import { getCookie } from '../../util/cookies';
 import { parseJson } from '../../util/json';
 
-export async function removeJerseyFromCart(jerseyId) {
+export async function removeJerseyFromCart(jerseyId, quantity) {
   const jerseyQuantityCookie = getCookie('cart');
 
   const jerseyQuantities = !jerseyQuantityCookie
     ? []
     : parseJson(jerseyQuantityCookie);
 
-  const jerseyToRemove = jerseyQuantities.findIndex((obj) => {
+  const jerseyIndex = jerseyQuantities.findIndex((obj) => {
     return obj.id === jerseyId;
   });
 
-  jerseyQuantities.splice(jerseyToRemove, 1);
+  if (jerseyIndex !== -1) {
+    const jersey = jerseyQuantities[jerseyIndex];
+    if (jersey.quantity > quantity) {
+      jersey.quantity -= quantity;
+    } else {
+      jerseyQuantities.splice(jerseyIndex, 1);
+    }
+  }
 
   await cookies().set('cart', JSON.stringify(jerseyQuantities));
 }
